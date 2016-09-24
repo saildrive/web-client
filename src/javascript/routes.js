39ae10cca212f4f3f2496ea1ee1125415e629fcc
Jared Rada <jared.rada@symphony.com>
2016-09-23 20:34:40 -0400

    add pilot page (#17)

--- src/javascript/routes.js
+++ src/javascript/routes.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { IndexRoute, Route } from 'react-router'
-import App from './containers/App/App'
-import Home from './containers/Home/Home'
-import Lights from "./containers/Lights/Lights"
+import React from "react";
+import { IndexRoute, Route } from "react-router";
+import App from "./containers/App/App";
+import Home from "./containers/Home/Home";
+import Lights from "./containers/Lights/Lights";
+import Pilot from "./containers/Pilot/Pilot";
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={Home} />
         <Route path="/lights" component={Lights} />
+        <Route path="/pilot" component={Pilot} />
     </Route>
 )
